85b0f6bc2634f33dbde61e937d42e5871ccc81df
Parsa <64251443+debug420@users.noreply.github.com>
2021-08-03 18:57:44 +1000

    Update CreateESPModule.lua

--- Exclusives-V2/Modules/CreateESPModule.lua
+++ Exclusives-V2/Modules/CreateESPModule.lua
@@ -1,83 +1,80 @@
-return function(ezlib, section)
-    ezlib:NewTitle(section, "ESP Engine V4")
-    ezlib:NewDiv(section)
-    
-    local espConfig;
-    if _G.ESPModule then
-        espConfig = loadstring(_G.ESPModule)()
-    else
-        espConfig = loadstring(game:HttpGet("https://raw.githubusercontent.com/debug420/Ez-Hub/master/Exclusives-V2/Modules/ESPModule.lua"))()
-    end
+return {
+    newESPTab = function(mainGUIInstance)
+        local espModule = loadstring(_G["EzHubModules"]["espmodule"])();
+        local tab = mainGUIInstance.newTab("ESP");
+        tab.newTitle("ESP Engine V4");
+        tab.newDiv();
 
-    ezlib:NewCheckBox(section, "Enabled", espConfig.Enabled, function(state)
-        espConfig.Enabled = state
-    end)
-    
-    ezlib:NewDiv(section)
-    ezlib:NewTitle(section, "ESP Elements")
-    
-    ezlib:NewCheckBox(section, "Tracer", espConfig.tracer, function(state)
-        espConfig.tracer = state
-    end)
-    
-    ezlib:NewCheckBox(section, "Headdot", espConfig.headdot, function(state)
-        espConfig.headdot = state
-    end)
-    
-    ezlib:NewCheckBox(section, "Info Tag", espConfig.tag, function(state)
-        espConfig.tag = state
-    end)
-    
-    ezlib:NewDiv(section)
-    ezlib:NewTitle(section, "ESP Behaviour")
-    
-    ezlib:NewCheckBox(section, "Lock to cursor", espConfig.locktocursor, function(state)
-        espConfig.locktocursor = state
-    end)
-    
-    ezlib:NewCheckBox(section, "Teamcheck", espConfig.teamcheck, function(state)
-        espConfig.teamcheck = state
-    end)
-    
-    ezlib:NewSlider(section, "Render Range", espConfig.renderrange, 100, 5000, function(val)
-        espConfig.renderrange = val
-    end)
-    
-    ezlib:NewDiv(section)
-    ezlib:NewTitle(section, "ESP Colors")
-    
-    ezlib:NewCheckBox(section, "Rainbow", espConfig.rainbowcolor, function(state)
-        espConfig.rainbowcolor = state
-    end)
-    
-    ezlib:NewSlider(section, "Rainbow Speed", espConfig.rainbowspeed, 1, 5, function(val)
-        espConfig.rainbowspeed = val
-    end)
-    
-    ezlib:NewCheckBox(section, "Team Color", espConfig.teamcolor, function(state)
-        espConfig.teamcolor = state
-    end)
-    
-    ezlib:NewDiv(section)
-    ezlib:NewTitle(section, "ESP SetColor")
-    ezlib:NewSlider(section, "R", espConfig.setcolor[1], 0, 255, function(val)
-        espConfig.setcolor[1] = val
-    end)
-    ezlib:NewSlider(section, "G", espConfig.setcolor[2], 0, 255, function(val)
-        espConfig.setcolor[2] = val
-    end)
-    ezlib:NewSlider(section, "B", espConfig.setcolor[3], 0, 255, function(val)
-        espConfig.setcolor[3] = val
-    end)
-    
-    ezlib:NewDiv(section)
-    ezlib:NewTitle(section, "Headdot Settings")
-    
-    ezlib:NewCheckBox(section, "Headdot Filled", espConfig.HeaddotFilled, function(state)
-        espConfig.HeaddotFilled = state
-    end)
-    
-    ezlib:NewSlider(section, "Headdot Scale", espConfig.HeaddotScale * 2, 1, 10, function(val)
-        espConfig.HeaddotScale = val / 2
-    end)
-end
+        tab.newCheckbox("Enabled", espModule.Enabled, function(state)
+            espModule.Enabled = state;
+        end)
+        
+        tab.newDiv();
+        tab.newTitle("ESP Elements");
+        
+        tab.newCheckbox("Tracer", espModule.tracer, function(state)
+            espModule.tracer = state;
+        end)
+        
+        tab.newCheckbox("Headdot", espModule.headdot, function(state)
+            espModule.headdot = state;
+        end)
+        
+        tab.newCheckbox("Info Tag", espModule.tag, function(state)
+            espModule.tag = state;
+        end)
+        
+        tab.newDiv();
+        tab.newTitle("ESP Behaviour");
+        
+        tab.newCheckbox("Lock to cursor", espModule.locktocursor, function(state)
+            espModule.locktocursor = state;
+        end)
+        
+        tab.newCheckbox("Teamcheck", espModule.teamcheck, function(state)
+            espModule.teamcheck = state;
+        end)
+        
+        tab.newSlider("Render Range", espModule.renderrange, 100, 5000, function(val)
+            espModule.renderrange = val;
+        end)
+        
+        tab.newDiv();
+        tab.newTitle("ESP Colors");
+        
+        tab.newCheckbox("Rainbow", espModule.rainbowcolor, function(state)
+            espModule.rainbowcolor = state;
+        end)
+        
+        tab.newSlider("Rainbow Speed", espModule.rainbowspeed, 1, 5, function(val)
+            espModule.rainbowspeed = val;
+        end)
+        
+        tab.newCheckbox("Team Color", espModule.teamcolor, function(state)
+            espModule.teamcolor = state;
+        end)
+        
+        tab.newDiv();
+        tab.newTitle("ESP Default Color");
+        tab.newSlider("R", espModule.setcolor[1], 0, 255, function(val)
+            espModule.setcolor[1] = val;
+        end)
+        tab.newSlider("G", espModule.setcolor[2], 0, 255, function(val)
+            espModule.setcolor[2] = val;
+        end)
+        tab.newSlider("B", espModule.setcolor[3], 0, 255, function(val)
+            espModule.setcolor[3] = val;
+        end)
+        
+        tab.newDiv();
+        tab.newTitle("Headdot Settings");
+        
+        tab.newCheckbox("Headdot Filled", espModule.HeaddotFilled, function(state)
+            espModule.HeaddotFilled = state;
+        end)
+        
+        tab.newSlider("Headdot Scale", espModule.HeaddotScale * 2, 1, 10, function(val)
+            espModule.HeaddotScale = val / 2;
+        end)
+    end
+}
