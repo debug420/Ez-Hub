b8dd9ffe77e0744f753ae46ddd716357c011a5ba
Parsa <64251443+debug420@users.noreply.github.com>
2021-09-17 12:44:44 +1000

    Update Arsenal.lua

--- Exclusives-V2/Arsenal.lua
+++ Exclusives-V2/Arsenal.lua
@@ -20,72 +20,77 @@ local weaponModTab = mainGUI.newTab("Gun Mods");
 weaponModTab.newTitle("Gun Mods");
 weaponModTab.newDiv();
 
-local infAmmo = false;
-local noRecoil = false;
-local automaticMode = false;
-local noSpread = false;
-local fireRate = false;
+if syn then
 
-local funcEnv, updateInventory;
-for i,v in pairs(getgc(true)) do
-    if type(v) == 'table' and rawget(v,'updateInventory') and rawget(v,'firebullet') then
-        funcEnv = getfenv(v.firebullet);
-        updateInventory = v.updateInventory;
+    local infAmmo = false;
+    local noRecoil = false;
+    local automaticMode = false;
+    local noSpread = false;
+    local fireRate = false;
+
+    local funcEnv, updateInventory;
+    for i,v in pairs(getgc(true)) do
+        if type(v) == 'table' and rawget(v,'updateInventory') and rawget(v,'firebullet') then
+            funcEnv = getfenv(v.firebullet);
+            updateInventory = v.updateInventory;
+        end
     end
+
+    -- Namecall metamethod in charge of wallbang
+    local oldNamecall, wallbangEnabled;
+    oldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
+        if tostring(getnamecallmethod()) == "FindPartOnRayWithIgnoreList" then
+            if wallbangEnabled then table.insert(({...})[2], workspace.Map); end
+        end
+        return oldNamecall(self, ...);
+    end)
+
+    game:GetService("RunService").Stepped:Connect(function()
+        if fireRate then
+            funcEnv.DISABLED = false;
+            funcEnv.DISABLED2 = false;
+        end
+        if noRecoil then
+            funcEnv.recoil = 0;
+        end
+        if infAmmo then
+            debug.setupvalue(updateInventory, 3, 70);
+        end
+        if automaticMode then
+            funcEnv.mode = 'automatic';
+        end
+        if noSpread then
+            funcEnv.currentspread = 0;
+            funcEnv.spreadmodifier = 0;
+        end
+    end)
+
+    weaponModTab.newCheckbox("No Recoil", noRecoil, function(state)
+        noRecoil = state;
+    end)
+
+    weaponModTab.newCheckbox("Automatic", automaticMode, function(state)
+        automaticMode = state;
+    end)
+
+    weaponModTab.newCheckbox("No Spread", noSpread, function(state)
+        noSpread = state;
+    end)
+
+    weaponModTab.newCheckbox("Fire Rate", fireRate, function(state)
+        fireRate = state;
+    end)
+
+    -- Toggles wallbang
+    weaponModTab.newCheckbox("Wallbang", false, function(state)
+        wallbangEnabled = state;
+        coroutine.wrap(function()
+            ezlib.newNotif(ezlib.enum.notifType.text, (state or "Enabled" and "Disabled").." wallbang successfully.").play().delete();
+        end)();
+    end)
+else
+    weaponModTab.newDesc("Your exploit does not support gun mods.");
 end
-
-game:GetService("RunService").Stepped:Connect(function()
-    if fireRate then
-        funcEnv.DISABLED = false;
-        funcEnv.DISABLED2 = false;
-    end
-    if noRecoil then
-        funcEnv.recoil = 0;
-    end
-    if infAmmo then
-        debug.setupvalue(updateInventory, 3, 70);
-    end
-    if automaticMode then
-        funcEnv.mode = 'automatic';
-    end
-    if noSpread then
-        funcEnv.currentspread = 0;
-        funcEnv.spreadmodifier = 0;
-    end
-end)
-
-weaponModTab.newCheckbox("No Recoil", noRecoil, function(state)
-	noRecoil = state;
-end)
-
-weaponModTab.newCheckbox("Automatic", automaticMode, function(state)
-	automaticMode = state;
-end)
-
-weaponModTab.newCheckbox("No Spread", noSpread, function(state)
-	noSpread = state;
-end)
-
-weaponModTab.newCheckbox("Fire Rate", fireRate, function(state)
-	fireRate = state;
-end)
-
--- Namecall metamethod in charge of wallbang
-local oldNamecall, wallbangEnabled;
-oldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
-    if tostring(getnamecallmethod()) == "FindPartOnRayWithIgnoreList" then
-        if wallbangEnabled then table.insert(({...})[2], workspace.Map); end
-    end
-    return oldNamecall(self, ...);
-end)
-
--- Toggles wallbang
-weaponModTab.newCheckbox("Wallbang", false, function(state)
-    wallbangEnabled = state;
-    coroutine.wrap(function()
-        ezlib.newNotif(ezlib.enum.notifType.text, (state or "Enabled" and "Disabled").." wallbang successfully.").play().delete();
-    end)();
-end)
 
 ----------------------------------------------------------------------
 
