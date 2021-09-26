a585591fa4e6643651054d2fb2388bb359bd0d45
Parsa <64251443+debug420@users.noreply.github.com>
2021-09-26 19:17:11 +1000

    Update Arsenal.lua

--- Exclusives-V2/Arsenal.lua
+++ Exclusives-V2/Arsenal.lua
@@ -1,41 +1,71 @@
-----------------------------------------------------------------------
-
 -- Arsenal Script
 
 local ezlib = loadstring(_G["EzHubModules"]["ezlib"])();
 local mainGUI = ezlib.create("Arsenal", nil, nil, nil, 286090429);
-
-----------------------------------------------------------------------
-
--- Initialize Sections
-
 loadstring(_G["EzHubModules"]["createaimbotmodule"])().newAimbotTab(mainGUI);
 loadstring(_G["EzHubModules"]["createespmodule"])().newESPTab(mainGUI);
 local weaponModTab = mainGUI.newTab("Gun Mods");
 
 ----------------------------------------------------------------------
-
 -- Gun Section
 
 weaponModTab.newTitle("Gun Mods");
 weaponModTab.newDiv();
 
-if syn then
+local infAmmo = false;
+local noRecoil = false;
+local automaticMode = false;
+local noSpread = false;
+local fireRate = false;
 
-    local infAmmo = false;
-    local noRecoil = false;
-    local automaticMode = false;
-    local noSpread = false;
-    local fireRate = false;
+local weaponModule = require(game.Players.LocalPlayer.PlayerGui.GUI.Client.Functions.Weapons);
+local fireBulletEnv = getfenv(weaponModule.firebullet);
 
-    local funcEnv, updateInventory;
-    for i,v in pairs(getgc(true)) do
-        if type(v) == 'table' and rawget(v,'updateInventory') and rawget(v,'firebullet') then
-            funcEnv = getfenv(v.firebullet);
-            updateInventory = v.updateInventory;
-        end
+local oldFirebullet;
+local function bulletFired(...)
+
+    if noSpread and fireBulletEnv.spreadmodifier and fireBulletEnv.currentspread then
+        pcall(function() fireBulletEnv.currentspread.Value = 0;
+        fireBulletEnv.spreadmodifier.Value = 0; end)
     end
 
+    if automaticMode and fireBulletEnv.mode then
+        pcall(function() fireBulletEnv.mode = "automatic"; end)
+    end
+
+    if infAmmo and fireBulletEnv.ammocount2 and fireBulletEnv.ammocount then
+        pcall(function() fireBulletEnv.ammocount.Value = 69;
+        fireBulletEnv.ammocount2.Value = 69; end)
+    end
+
+    if noRecoil and fireBulletEnv.recoil then
+        pcall(function() fireBulletEnv.recoil = 0; end)
+    end
+
+	oldFirebullet(...);
+end
+
+oldFirebullet = hookfunction(weaponModule.firebullet, bulletFired);
+
+weaponModTab.newCheckbox("No Recoil", noRecoil, function(state)
+    noRecoil = state;
+end)
+
+weaponModTab.newCheckbox("Automatic", automaticMode, function(state)
+    automaticMode = state;
+end)
+
+weaponModTab.newCheckbox("Infinite Ammo", infAmmo, function(state)
+    infAmmo = state;
+end)
+
+weaponModTab.newCheckbox("No Spread", noSpread, function(state)
+    noSpread = state;
+end)
+
+weaponModTab.NewDiv();
+
+if syn then
     -- Namecall metamethod in charge of wallbang
     local oldNamecall, wallbangEnabled;
     oldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
@@ -43,42 +73,6 @@ if syn then
             if wallbangEnabled then table.insert(({...})[2], workspace.Map); end
         end
         return oldNamecall(self, ...);
-    end)
-
-    game:GetService("RunService").Stepped:Connect(function()
-        if fireRate then
-            funcEnv.DISABLED = false;
-            funcEnv.DISABLED2 = false;
-        end
-        if noRecoil then
-            funcEnv.recoil = 0;
-        end
-        if infAmmo then
-            debug.setupvalue(updateInventory, 3, 70);
-        end
-        if automaticMode then
-            funcEnv.mode = 'automatic';
-        end
-        if noSpread then
-            funcEnv.currentspread = 0;
-            funcEnv.spreadmodifier = 0;
-        end
-    end)
-
-    weaponModTab.newCheckbox("No Recoil", noRecoil, function(state)
-        noRecoil = state;
-    end)
-
-    weaponModTab.newCheckbox("Automatic", automaticMode, function(state)
-        automaticMode = state;
-    end)
-
-    weaponModTab.newCheckbox("No Spread", noSpread, function(state)
-        noSpread = state;
-    end)
-
-    weaponModTab.newCheckbox("Fire Rate", fireRate, function(state)
-        fireRate = state;
     end)
 
     -- Toggles wallbang
@@ -89,11 +83,10 @@ if syn then
         end)();
     end)
 else
-    weaponModTab.newDesc("Your exploit does not support gun mods.");
+    weaponModTab.newDesc("Your exploit does not support wallbang");
 end
 
 ----------------------------------------------------------------------
-
 -- Active
 
 mainGUI.openTab(mainGUI.getTab("Aimbot"));
