d44081cd3bf8174f2187ac8bb1c929242b43f785
Parsa <parsa419.usdf@gmail.com>
2021-10-03 13:07:13 +1100

    Update 4.1.2

--- Exclusives-V2/Universal.lua
+++ Exclusives-V2/Universal.lua
@@ -1,8 +1,36 @@
-----------------------------------------------------------------------
+local ezlib = loadstring(_G["EzHubModules"]["ezlib"])();
 
+-- Below is a list of games that the universal is not recommended to be ran on
+-- as there could be an exclusive specifically tuned and made for that game
+
+local listOfGames = {
+    ["Phantom Forces"] = 292439477,
+    ["DDay"] = 901793731,
+    ["Arsenal"] = 286090429,
+    ["Redwood Prison"] = 402122991,
+    ["Prison Life"] = 155615604,
+    ["Legends of Speed"] = 3101667897,
+    ["Mad City"] = 1224212277,
+    ["Shindo"] = 4616652839,
+    ["Rise of Nations"] = 2569453732,
+    ["Bloxburg"] = 185655149,
+    ["Wild West"] = 2317712696,
+    ["Jailbreak"] = 606849621
+}
+
+for i,v in pairs(listOfGames) do
+    if game.PlaceId == v then
+        coroutine.wrap(function()
+            ezlib.newNotif(ezlib.enum.notifType.longText,
+                "The following game is not recommended for Universal use. Look in the exclusives V2 section for a script specifically made for "..i.."."
+                ).play().delete();
+        end)();
+    end
+end
+
+----------------------------------------------------------------------
 -- Universal Script
 
-local ezlib = loadstring(_G["EzHubModules"]["ezlib"])();
 local mainGUI = ezlib.create("Universal");
 
 -- Init tabs
